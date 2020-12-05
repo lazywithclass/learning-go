412973dbb6ce956704597cc4d7712f38abb940de
Alberto Zaccagni <montecristoh@gmail.com>
2020-12-05 20:35:05 +0100

    moar go

--- clessidra.go
+++ clessidra.go
@@ -1,3 +1,36 @@
+/*
+Figure: Clessidra
+=================
+
+Scrivere un programma clessidra.go, dotato di una funzione
+ricorsiva 
+
+clessidra(n, offset int) 
+
+che emetta nel flusso d'uscita una clessidra di base 2*n-1
+e una funzione main() per testarla.
+
+Ad esempio, una clessidra di dimensione 1 e`:
+*
+
+Una clessidra di dimensione 2 e`:
+***
+ *
+***
+
+cioè una clessidra di dimensione 1, con offset 1, inclusa tra due basi di lughezza ?
+
+Una clessidra di dimensione 3 e`:
+*****
+ ***
+  *
+ ***
+*****
+
+cioè una clessidra di dimensione 2, con offset 1, inclusa tra due basi di lughezza ?
+che è a sua volta ...
+*/
+
 package main
 
 import (
