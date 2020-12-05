2dcef39c7c8dd439cf7c825bdd184e213dd5bb24
Alberto Zaccagni <montecristoh@gmail.com>
2020-12-05 18:05:38 +0100

    moar go

--- clessidra.go
+++ clessidra.go
@@ -5,25 +5,21 @@ import (
 )
 
 func main() {
-	clessidra(9, 0)
+	clessidra(3, 0)
 }
 
 func clessidra(n, offset int) {
-	if n == 0 {
-		return
-	}
-
 	if n == 1 {
 		printBlanks(offset)
 		printStars((2 * n) - 1)
-		clessidra(n - 1, offset + 1)
-	} else {
-		printBlanks(offset)
-		printStars((2 * n) - 1)
-		clessidra(n - 1, offset + 1)
-		printBlanks(offset)
-		printStars((2 * n) - 1)
+		return
 	}
+
+	printBlanks(offset)
+	printStars((2 * n) - 1)
+	clessidra(n - 1, offset + 1)
+	printBlanks(offset)
+	printStars((2 * n) - 1)
 }
 
 func printBlanks(n int) {
@@ -33,9 +29,6 @@ func printBlanks(n int) {
 }
 
 func printStars(n int) {
-	if n == 0 {
-		n = 1
-	}
 	for i := 0; i < n; i++ {
 		fmt.Print("*")
 	}
